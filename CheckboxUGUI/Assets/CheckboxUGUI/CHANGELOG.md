fc026839905484a96392fded24b285b5af232e85
DAIKI <daiki.evilone@gmail.com>
2025-01-26 12:49:58 +0900

    Added create menu.

--- CheckboxUGUI/Assets/CheckboxUGUI/CHANGELOG.md
+++ CheckboxUGUI/Assets/CheckboxUGUI/CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 
-## [0.5.0] - 2025-01-26
+## [0.5.1] - 2025-01-26
+### Added
+- Added create menu.
+
+## [0.5.0] - 2025-01-25
 ### Added
 - Added Checkbox.
 - Added Radio button option.
---
+++ CheckboxUGUI/Assets/CheckboxUGUI/Scripts/Editor/UICreateUtility.cs
@@ -0,0 +1,58 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CheckboxUGUI.Editor
+{
+    public static class UICreateUtility
+    {
+        private const string kStandardSpritePath = "UI/Skin/UISprite.psd";
+        private const string kBackgroundSpritePath = "UI/Skin/Background.psd";
+        private const string kInputFieldBackgroundPath = "UI/Skin/InputFieldBackground.psd";
+        private const string kKnobPath = "UI/Skin/Knob.psd";
+        private const string kCheckmarkPath = "UI/Skin/Checkmark.psd";
+        private const string kDropdownArrowPath = "UI/Skin/DropdownArrow.psd";
+        private const string kMaskPath = "UI/Skin/UIMask.psd";
+
+        private static DefaultControls.Resources s_StandardResources;
+
+        public static DefaultControls.Resources GetStandardResources()
+        {
+            if (s_StandardResources.standard == null)
+            {
+                s_StandardResources.standard = AssetDatabase.GetBuiltinExtraResource<Sprite>(kStandardSpritePath);
+                s_StandardResources.background = AssetDatabase.GetBuiltinExtraResource<Sprite>(kBackgroundSpritePath);
+                s_StandardResources.inputField = AssetDatabase.GetBuiltinExtraResource<Sprite>(kInputFieldBackgroundPath);
+                s_StandardResources.knob = AssetDatabase.GetBuiltinExtraResource<Sprite>(kKnobPath);
+                s_StandardResources.checkmark = AssetDatabase.GetBuiltinExtraResource<Sprite>(kCheckmarkPath);
+                s_StandardResources.dropdown = AssetDatabase.GetBuiltinExtraResource<Sprite>(kDropdownArrowPath);
+                s_StandardResources.mask = AssetDatabase.GetBuiltinExtraResource<Sprite>(kMaskPath);
+            }
+            return s_StandardResources;
+        }
+
+        public static GameObject FindUIPlacement(GameObject selectedObject)
+        {
+            if (selectedObject != null && selectedObject.GetComponentInParent<Canvas>(true) != null)
+            {
+                return selectedObject;
+            }
+
+            Canvas canvas = Object.FindAnyObjectByType<Canvas>(FindObjectsInactive.Include);
+            if (canvas != null)
+            {
+                return canvas.gameObject;
+            }
+
+            return null;
+        }
+
+        public static void SetLayerRecursively(GameObject go, int layer)
+        {
+            go.layer = layer;
+            Transform t = go.transform;
+            for (int i = 0; i < t.childCount; i++)
+                SetLayerRecursively(t.GetChild(i).gameObject, layer);
+        }
+    }
+}
